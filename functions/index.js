48f2cb7b23293fdd4ec954564c47d8065b3fa74e
Austin <aripang.com@gmail.com>
2020-08-06 05:12:40 +0800

    configured functions;

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,23 @@
 const functions = require('firebase-functions');
-
 // The Firebase Admin SDK to access Cloud Firestore.
 const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
 
 admin.initializeApp();
 
 // Using existing Express apps
-
-const express = require('express');
-const cors = require('cors');
 
 const app = express();
 
 app.use(cors({ origin: true }));
 
 app.get('/user', (req, res) => {
-    res.status(200).json({
-        status: true,
-        data: "sdf"
-    });
+  const response = {
+    status: true,
+    data: 'sdf',
+  };
+  res.status(200).json(response);
 });
 
-
 exports.api = functions.https.onRequest(app);
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   response.send('Hello from Firebase!');
-// });
